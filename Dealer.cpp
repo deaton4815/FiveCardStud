f8b09c0ab278285c337bf8f797263a78747d8743
Philip Deaton <philipdeaton826@gmail.com>
2024-04-27 13:45:41 -0400

    Starting dealer functionality

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,1 +1,22 @@
 #include "Dealer.h"
+
+using namespace std;
+
+void Dealer::FiveCardStud() {
+
+	dealCards();
+	vector <unsigned> keepers = getKeepers();
+	m_hand.setDiscards(keepers);
+
+}
+
+void Dealer::dealCards() {
+	vector<Card> dealt = m_deck.getCards(m_hand.getHandSize());
+	m_hand.newHand(dealt);
+}
+
+// TO BE REPLACED
+vector<unsigned> Dealer::getKeepers() {
+	vector<unsigned> keepers{ 1,3 };
+	return keepers;
+}
--- Dealer.h
+++ Dealer.h
@@ -1,5 +1,24 @@
 #pragma once
+
+#include <vector>
+
+#include "Card.h"
+#include "CardDeck.h"
+#include "Hand.h"
+
 class Dealer
 {
+public:
+
+	void FiveCardStud();
+
+private:
+	
+	CardDeck m_deck;
+	Hand m_hand;
+
+	void dealCards();
+
+	std::vector<unsigned> getKeepers();
 };
 
